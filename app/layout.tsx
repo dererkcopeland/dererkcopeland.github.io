3e5b77356f848cce08b7761f3a20c90b61d0afdd
dererkcopeland <125219246+dererkcopeland@users.noreply.github.com>
2025-02-26 17:41:00 -0500

    Hero Section
    Initialize Next.js project with essential configurations and components

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/app/provider";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Dererk Copeland | Portfolio",
+  description: "Modern & Minimalist",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
