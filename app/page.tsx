8b10b09b98d401713c8608edccfece2e1372a5d9
dererkcopeland <125219246+dererkcopeland@users.noreply.github.com>
2025-03-10 20:20:29 -0400

    Updated Email

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import { usePathname, useSearchParams } from "next/navigation";
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
-import Clients from "@/components/Clients";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import Education from "@/components/Education";
